668b78bfba3e107dcfd91dbba843d833f0bafa15
UwUnyaa <UwUnyaa@outlook.com>
2020-08-19 21:16:39 +0200

    add a copyright notice

--- presentation-mode.el
+++ presentation-mode.el
@@ -6,6 +6,8 @@
 
 ;;; license: GPLv3 or newer
 
+;; Copyright (C) 2019, 2020 UwUnyaa
+
 ;; This program is free software: you can redistribute it and/or modify it
 ;; under the terms of the GNU General Public License as published by the Free
 ;; Software Foundation, either version 3 of the License, or (at your option)
